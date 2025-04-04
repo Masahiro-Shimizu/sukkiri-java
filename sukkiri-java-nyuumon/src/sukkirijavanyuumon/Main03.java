a905c9748b4fa9d12c39a3ba68e9b4b7a3b3fffd
masahiro.shimizu <uw0665@gmail.com>
2025-04-04 09:33:49 +0900

    chapter00 complete

---
+++ sukkiri-java-nyuumon/src/sukkirijavanyuumon/Main03.java
@@ -0,0 +1,8 @@
+package sukkirijavanyuumon;
+
+public class Main03 {
+	public static void main(String[] args) {
+		System.out.println("湊くんかっこいい！最高！"); //最高！の後の”を忘れないようにする
+	}
+
+}
---
+++ sukkiri-java-nyuumon/src/sukkirijavanyuumon/Main04.java
@@ -0,0 +1,10 @@
+package sukkirijavanyuumon;
+
+public class Main04 {
+	public static void main(String[] args) {
+		System.out.println("菅原拓真です");
+		System.out.println("31歳です");
+		System.out.println("お酒が好きです");
+	}
+
+}
